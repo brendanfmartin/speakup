5361f3de7431217ac096f6cfc8c001e9c0d3a617
brendanfmartin <brendanfmartin@gmail.com>
2020-06-19 21:44:07 -0400

    Parsing and encriching data

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 const speak_up_lib = {
-  select: (e) => {
-    e.preventDefault();
-    console.log(e.path[0].value);
-  },
-
-  getData: () => {
-    fetch('https://www.google.com')
-       .then(res => speak_up_data.res = res)
+  getReps: () => {
+    const zip = speak_up_dom.zip().value;
+    const key = 'AIzaSyB7qmZN41K4jxK8vqWNuPdurpFfIFvu8_0';
+    const url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${zip}`;
+    fetch(url)
+       .then(res => {
+         if (res.status > 299) {
+           speak_up_lib.showError();
+         } else {
+           res.json().then(speak_up_lib.parseData)
+         }
+       })
        .catch(err => {
          console.error(err);
-         speak_up_data.error = err;
+         speak_up_lib.showError();
        });
+  },
+
+  showError: () => {
+    speak_up_dom.notification().classList.remove('hidden');
+  },
+
+  closeError: () => {
+    speak_up_dom.notification().classList.add('hidden');
+  },
+
+  parseData: (data) => {
+    const localOfficeKeys = ['administrativeArea1', 'administrativeArea2', 'locality', 'regional', 'special', 'subLocality1', 'subLocality2'];
+    const localOffices = [];
+
+    /**
+     * get only the local offices
+     */
+    data.offices.forEach((office) => {
+      if (localOfficeKeys.indexOf(office.levels[0]) > -1) {
+        localOffices.push(Object.assign({}, office));
+      }
+    });
+
+    /**
+     * enrich offices with current officials
+     */
+    localOffices.forEach((office) => {
+      office.officials = [];
+      office.officialIndices.forEach(i => {
+        office.officials.push(data.officials[i]);
+      });
+    });
+
+    console.log(localOffices);
+
   }
 };
 
-const speak_up_data = {
-  error: undefined,
-  res: undefined
+const speak_up_dom = {
+  notification: () => document.getElementById('notification-container'),
+  zip: () => document.getElementById('zip')
 };
-
-(() => {
-  // speak_up_lib.getData()
-})();
---
+++ todo.md
@@ -0,0 +1,18 @@
+# todo 
+
+## API
+ - restrict api key
+ - proxy through apis
+ - google analytics
+
+## UI
+ - icon
+ - favico
+ - fonts
+ - dropdown districts
+
+## hosting
+ - s3 bucket
+ - url
+ - deployment
+ - caching
