394664c5c769e3a63153bdac1e20e8c6db8285c8
brendanfmartin <brendanfmartin@gmail.com>
2020-06-23 19:06:15 -0400

    Pulling more info

--- js/main.js
+++ js/main.js
@@ -2,22 +2,29 @@ const speak_up_lib = {
   craft_email: () => {
     const matter = speak_up_lib.get_radio_value('matter');
     const official = speak_up_lib.get_radio_value('official');
+    console.log(matter)
+    console.log(speak_up_maps.official[official]);
     const mailto = speak_up_maps.official[official].mailto;
-    const subject = `${speak_up_maps.official[official].subject} - ${speak_up_maps.matter[matter].subject}`;
+    const subject = `${speak_up_maps.matter[matter].subject}`;
     const body = `${speak_up_maps.matter[matter].body} ${speak_up_maps.official[official].body}`;
     window.location.href = `mailto:${mailto}?body=${encodeURIComponent(body)}&subject=${encodeURIComponent(subject)}`;
   },
 
   select_matter: () =>{
+    const matter = speak_up_lib.get_radio_value('matter');
     document.getElementById('official-list').hidden = false;
-    speak_up_lib.show_officials();
+    speak_up_lib.show_officials(matter);
   },
 
-  show_officials: () => {
-      const radio = `
+  show_officials: (matter) => {
+    const officials = [];
+    speak_up_maps.matter[matter].officials.map(o => officials.push(speak_up_maps.official[o]));
+    console.log(officials)
+    const radios = [];
+    const radio = `
       <div>
-        <input onclick="speak_up_lib.select_official()" type="radio" id="thomas_murt" name="official" value="thomas_murt">
-        <label for="thomas_murt">PA House of Rep Thomas P. Murt</label>
+        <input onclick="speak_up_lib.select_official()" type="radio" id="${officials[0].id}" name="official" value="${officials[0].id}">
+        <label for="${officials[0].id}">${officials[0].name}</label>
       </div>`;
       document.getElementById('official-list-dynamic').innerHTML = radio;
   },
@@ -52,23 +59,22 @@ const speak_up_maps = {
     jim_kenney: {
       mailto: 'jim_kenney@pagov.com',
       body: ' And fuck you Jim Kenney',
-      subject: 'Jim Kenney'
     },
     pat_toomey: {
       mailto: 'pat_toomey@pagov.com',
       body: ' And fuck you Pat Toomey',
-      subject: 'Pat Toomey'
     },
     thomas_murt: {
       mailto: 'thomas_murt@pagov.com',
       body: ' And fuck you Thomas Murt',
-      subject: 'Thomas Murt'
     },
     robert_schroeder: {
       mailto: 'robert.schroeder@louisvilleky.gov',
       body: 'tbd',
       tel: '5025747111',
-      phone_text: 'Remain calm'
+      phone_text: 'Remain calm',
+      name: 'Interim Chief of Police Robert Schroeder',
+      id: 'robert_schroeder'
     }
   }
 };
